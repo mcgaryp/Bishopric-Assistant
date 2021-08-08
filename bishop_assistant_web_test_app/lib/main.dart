f995c721abeb3305f87653664667af127b9596f7
Porter McGary <mcg15003@byui.edu>
2021-08-08 12:55:58 -0600

    initializing fire base properly in the web app

--- bishop_assistant_web_test_app/lib/main.dart
+++ bishop_assistant_web_test_app/lib/main.dart
@@ -25,23 +25,26 @@ class _AppState extends State<App> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        if (snapshot.hasError) {
-          return SomethingWentWrong();
-        }
+    return MaterialApp(
+      theme: ThemeData(),
+      home: FutureBuilder(
+        // Initialize FlutterFire:
+        future: _initialization,
+        builder: (context, snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return SomethingWentWrong();
+          }
 
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MyApp();
-        }
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MyApp();
+          }
 
-        // Otherwise, show something whilst waiting for initialization to complete
-        return Loading();
-      },
+          // Otherwise, show something whilst waiting for initialization to complete
+          return Loading();
+        },
+      ),
     );
   }
 }
